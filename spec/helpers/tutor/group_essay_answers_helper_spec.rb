d1ef1469936a4bb5804a65e809afd05f1e447673
torasakura <sarah@rapidrabbit.de>
2012-04-02 21:26:28 +0200

    bug fixes

---
+++ spec/helpers/tutor/group_essay_answers_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the Tutor::GroupEssayAnswersHelper. For example:
+#
+# describe Tutor::GroupEssayAnswersHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe Tutor::GroupEssayAnswersHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
