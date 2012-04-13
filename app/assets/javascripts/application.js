6bea1d819e08fba476d9394c116739a1296500dc
luki3k5 <luki3k5@gmail.com>
2012-04-13 22:05:17 +0200

    channels are coming back now from server when we click name on the roster

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,15 @@
 //= require jquery_ujs
 //= require_self
 //= require_directory . 
+//
+
+
+$(function() {
+  $('[data-remote][data-replace]')
+    .data('type', 'html')
+    .live('ajax:success', function(event, data) {
+      var $this = $(this);
+      $($this.data('replace')).html(data);
+      $this.trigger('ajax:replaced');
+    });
+});
