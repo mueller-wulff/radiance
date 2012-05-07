387b14e10cb997979c58d1e62ae60871016c623e
torasakura <sarah@rapidrabbit.de>
2012-05-07 10:53:45 +0200

    issue #21 #26 bug fix with displaying deadlines and group essay answers

--- app/models/group_essay_answer.rb
+++ app/models/group_essay_answer.rb
@@ -2,4 +2,11 @@ class GroupEssayAnswer < ActiveRecord::Base
   has_paper_trail
   belongs_to :group_essay
   belongs_to :group
+  
+  def self.answered_by_group?(student, element)
+    group = student.find_tutor_of_course(element.content.page.course)
+    return true unless GroupEssayAnswer.where(:group_id => group.id, :group_essay_id => element.id).empty?
+    return false
+  end
+  
 end
