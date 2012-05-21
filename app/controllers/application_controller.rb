35882c7d3bef4a70c9a21e19d5638a07b61998ab
torasakura <sarah@rapidrabbit.de>
2012-05-21 17:17:21 +0200

    scroll to button issue #53 localized faqs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = params[:locale] if params.include?('locale')
   end
+  
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
 
   private
   
